d50c03ce01757823ccdac92f8417a23627bd9e9f
kenleytomlin <kenleytomlin@gmail.com>
2018-09-25 15:39:40 +0100

    fix(app/templates/package.json.tpl): Add build to scripts, add missing fs-extra
    The build script was missing from package.json scripts, fs-extra was missing as a dev dependency
    causing the build script to fail.

--- __tests__/app.js
+++ __tests__/app.js
@@ -35,6 +35,7 @@ describe('generator-seedrs-react:app', () => {
         name: 'seedrs-react-project',
         scripts: {
           start: 'webpack-dev-server --config config/webpack.development.js --hot',
+          build: 'NODE_ENV=production node scripts/build.js',
           lint: 'eslint src/**/*.js',
           test: 'node scripts/test.js --env=jsdom'
         },
@@ -70,6 +71,7 @@ describe('generator-seedrs-react:app', () => {
           'eslint-plugin-import': '^2.11.0',
           'eslint-plugin-jest': '^21.15.1',
           'eslint-plugin-react': '^7.7.0',
+          'fs-extra': '^7.0.0',
           'html-webpack-plugin': '^3.2.0',
           jest: '^22.4.3',
           'react-hot-loader': '^4.3.8',
